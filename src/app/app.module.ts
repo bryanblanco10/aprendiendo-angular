7812a6726fd0aeeb9cd200cfaa5861c124555bd2
bryanblanco10 <ing.bryanblanco10@gmail.com>
2023-11-27 11:07:14 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HightlightDirective } from './directives/hightlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent,
+    ProductComponent,
+    ProductsComponent,
+    HeaderComponent,
+    ReversePipe,
+    TimeAgoPipe,
+    HightlightDirective,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
